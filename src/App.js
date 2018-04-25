bad17906a55f9fab21b175be1032295c33d822aa
Eva.Dinckel <evadinckel@gmail.com>
2018-04-25 12:27:01 +0100

    update the main app component with attempt at fetching data from third party API, in progress

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const API = 'https://jsonplaceholder.typicode.com/users';
+const DEFAULT_QUERY = 'redux';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users:[],
+      isLoading: false,
+      error: null,
+    };
+  }
+
+componentDidMount() {
+  this.setState({ isLoading: true });
+
+  fetch(API + DEFAULT_QUERY)
+  .then(response => {
+    if(response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Oops! Something went wrong!');
+    }
+  })
+  .then(data => this.setState(({ users: data.users, isLoading: false }))
+  .catch(error => this.setState({ error, isLoading: false }));
+}
+
+
   render() {
+    const { users, isLoading, error } = this.state;
+
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {users.map(user =>
+          <div key={user.objectID}</div>
+          <a href={user.url}>{user.title}</a>
+        </div>
+      )}
       </div>
     );
   }
